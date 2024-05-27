3aabc9b5820dd7ca7a5370ab7f70382dca312d22
AnettX <ann.badova@gmail.com>
2024-05-27 22:29:11 +0300

    update server.js

--- src/server.js
+++ src/server.js
@@ -19,9 +19,8 @@ export const setupServer = async () => {
     }),
   );
 
-  app.use('*', (req, res, next) => {
+  app.use('*', (req, res) => {
     res.status(404).json({ message: 'Not found' });
-    next();
   });
 
   dotenv.config();
